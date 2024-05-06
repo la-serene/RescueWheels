6a06965eea7af7362ab90665c04bd4e822c582b0
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-06 21:07:22 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./src/app.js")
+
+const PORT = 3055
+
+const server = app.listen( PORT, () => {
+    console.log(`Server starts at http://localhost:${PORT}`)
+})
+
+process.on("SIGINT", () => {
+    server.close( () => {
+        console.log("Exit Server Express.")
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+// init middlewares
+
+// init db
+
+// init routes
+
+// handling err
+
+module.exports = app
