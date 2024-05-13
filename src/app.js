1521491781e94dece4b2e26cb407e875b328633b
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-13 17:40:34 +0700

    add middlewares

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require("express")
+const morgan = require("morgan")
+const { default: helmet } = require("helmet");
+const compression = require("compression");
 const app = express()
 
 // init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
 
 // init db
 
