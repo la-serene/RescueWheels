063f9070ab211d58ae77c1b32051bd2069234e07
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-13 20:39:50 +0700

    connection handling methods

---
+++ src/helpers/check.connect.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const mongoose = require("mongoose")
+const os = require("os");
+const _SECONDS = 5000
+const countConnect = () => {
+    const numConnection = mongoose.connections.length
+    console.log(`Number of connection: ${numConnection}`)
+}
+
+const checkOverload = () => {
+    setInterval(() => {
+        const numConnection = mongoose.connections.length
+        const numCore = os.cpus().length
+        const memoryUsage = process.memoryUsage().rss
+        const maxConnection = numConnection + 5
+
+        if (numConnection > maxConnection) {
+            console.log("Connection overload detected")
+        }
+    }, _SECONDS)
+}
+
+module.exports = {
+    countConnect
+}
