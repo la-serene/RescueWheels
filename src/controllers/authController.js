62df867d2981312cd4218a14df5e6207e11d35be
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-24 04:01:51 +0700

    add signin API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/UserAccount.js"
 import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
 
 export const signUp = async (req, res) => {
     const { email, password, ...rest } = req.body
@@ -18,7 +19,27 @@ export const signUp = async (req, res) => {
         await user.save()
 
         res.status(200).json({
-            message: "Done"
+            message: "Successfully create new account!"
+        })
+    }
+}
+
+export const signIn = async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email: email })
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (isMatch) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '720h' });
+        res.status(200).json({
+            message: "Successfully sign in!",
+            token: token,
+            userId: user._id
+        })
+    } else {
+        res.status(400).json({
+            message: "Invalid email or password."
         })
     }
 }
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import multer from 'multer'
-
 import {
-    signUp
+    signUp,
+    signIn
 } from '../controllers/authController.js'
 
 const router = express.Router()
 const upload = multer()
 
-router.get("/signup", signUp)
+router.post("/signup", signUp)
+router.post("/signin", signIn)
 
 export default router
