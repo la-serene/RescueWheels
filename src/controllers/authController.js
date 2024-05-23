120f6eaac771585ea1f7a12a240e700fed9cf33e
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-24 03:02:25 +0700

    [UPDATE] signUp(): hash password before save to db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,35 +2,24 @@ import User from "../models/UserAccount.js"
 import bcrypt from "bcrypt"
 
 export const signUp = async (req, res) => {
-    const email = req.body.email
-    let password = req.body.password
+    const { email, password, ...rest } = req.body
 
-    try {
-        if (await User.findOne({email: email}).lean() === null) {
-            const salt = 10
-            bcrypt.hash(password, salt, (err, hash) => {
-                if (err) {
-                    console.error(err);
-                } else {
-                    password = hash
-                }
-            })
-            const user = new User({
-                email: email,
-                password: password,
-                ...req.body
-            })
-            await user.save()
+    if (await User.findOne({ email: email }) !== null) {
+        res.status(400).json({
+            message: "Email already exists"
+        })
+    } else {
+        const salt = 10
+        const user = new User({
+            email: email,
+            password: await bcrypt.hash(password, salt),
+            ...rest
+        })
+        console.log(user)
+        await user.save()
 
-            res.status(200).json({
-                message: "Successful sign up."
-            })
-        } else {
-            res.status(400).json({
-                message: "Email already exists."
-            })
-        }
-    } catch (err) {
-        console.error(err)
+        res.status(200).json({
+            message: "Done"
+        })
     }
 }
