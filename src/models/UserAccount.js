ca02d3b04c0ddca2df9eecae82de001dba26e58a
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-24 03:01:59 +0700

    [UPDATE] temporarily remove validator

--- src/models/UserAccount.js
+++ src/models/UserAccount.js
@@ -1,70 +1,34 @@
 import mongoose from "mongoose"
-import validator from "validator";
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         unique: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid email format');
-            }
-        }
     },
     password: {
         type: String,
         required: true,
-        validate(value) {
-            if (!validator.isAlphanumeric(value)) {
-                throw new Error('Invalid password format');
-            }
-        }
     },
     firstName: {
         type: String,
         required: true,
-        validate(value) {
-            if (!validator.isAlpha(value)) {
-                throw new Error('Invalid firstName format');
-            }
-        }
     },
     lastName: {
         type: String,
         required: true,
-        validate(value) {
-            if (!validator.isAlpha(value)) {
-                throw new Error('Invalid lastName format');
-            }
-        }
     },
     phoneNumber: {
         type: String,
         required: true,
         unique: true,
-        validate(value) {
-            if (!validator.isAlphanumeric(value)) {
-                throw new Error('Invalid phoneNumber format');
-            }
-        }
     },
     gender: {
         type: String,
         required: true,
-        validate(value) {
-            if (!validator.isAlpha(value)) {
-                throw new Error('Invalid gender format');
-            }
-        }
     },
     address: {
         type: String,
-        validate(value) {
-            if (!validator.isAlpha(value)) {
-                throw new Error('Invalid address format');
-            }
-        }
     },
     avatar: {
         type: String
