198a6ee50f500e89b537ddd3e52f3b6f3a763af2
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-24 23:07:51 +0700

    add route for provider

--- src/routes/providerRoute.js
+++ src/routes/providerRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import multer from 'multer'
 import {
-    signIn,
+    submitService,
 } from '../controllers/providerController.js'
 
 const router = express.Router()
 const upload = multer()
 
-router.post("/signin", signIn)
+router.post("/submitService/:providerId", submitService)
 
 export default router
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,9 @@
 import authRoute from "./authRoute.js"
 import userRoute from "./userRoute.js"
+import providerRoute from "./providerRoute.js"
 
 export default function route(app) {
     app.use('/', authRoute)
     app.use('/', userRoute)
+    app.use('/', providerRoute)
 }
