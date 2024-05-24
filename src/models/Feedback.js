9a8bf8de9d9b0f72e72033458317cf2d199f5c06
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-24 22:30:20 +0700

    feedback model

---
+++ src/models/Feedback.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const feedbackSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: ["User", "Provider"]
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: ["User", "Provider"]
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["feedback", "report"]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600,
+    }
+}, {
+    methods: {}
+})
+
+const Feedback = mongoose.model("Feedback", feedbackSchema)
+export default Feedback
