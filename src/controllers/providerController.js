5e4b50b51a3583643d4ae725b70ecc5c4f1d596c
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-25 03:14:59 +0700

    add provider features

--- src/controllers/providerController.js
+++ src/controllers/providerController.js
@@ -1,7 +1,55 @@
+import Service from "../models/Service.js";
+import PendingService from "../models/PendingService.js";
+
 export const submitService = async (req, res) => {
     const providerId = req.params.providerId
     const name = req.body.name
     const description = req.body.description
 
+    await new PendingService({
+        providerId: providerId,
+        name: name,
+        description: description
+    }).save()
 
+    res.status(200).json({
+        message: "Pending new service."
+    })
+}
+
+export const getServiceByQuantity = async (req, res) => {
+    const quantity = req.params.quantity
+    const services = await Service.find().limit(quantity)
+
+    res.status(200).json({
+        services: services,
+        message: `Successfully retrieve ${quantity} services.`
+    })
+}
+
+export const updateService = async (req, res) => {
+    const providerId = req.params.providerId
+    const serviceId = req.params.serviceId
+    const name = req.body.name
+    const description = req.body.description
+
+    await new PendingService({
+        from: providerId,
+        to: serviceId,
+        name: name,
+        description: description
+    }).save()
+
+    res.status(200).json({
+        message: "Pending service update."
+    })
+}
+
+export const deleteService = async (req, res) => {
+    const serviceId = req.params.serviceId
+    await Service.deleteOne({ _id: serviceId })
+
+    res.status(200).json({
+        message: "Service deleted."
+    })
 }
