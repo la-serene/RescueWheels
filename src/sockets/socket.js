ad39c947043620db50a308da617dc447783d0951
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-25 14:59:22 +0700

    [UPDATE] initializeSocket(): verify userId in handshake query

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -2,8 +2,24 @@ import chatHandler from "./chat.socket.js";
 import notificationHandler from "./notification.socket.js";
 
 export default function initializeSocket(io) {
+    io.use((socket, next) => {
+        const userId = socket.handshake.query.userId;
+        if (!userId) {
+            return next(new Error("Invalid userId"));
+        }
+        else {
+            socket.userId = socket.handshake.query.userId;
+            next();
+        }
+    })
+
     io.on('connection', (socket) => {
-        console.log(`A user with id ${socket.id} connected`);
+        socket.join(socket.userId);
+        console.log(`A user with id ${socket.userId} connected`);
+
+        socket.broadcast.emit("userConnected", {
+            userID: socket.id
+        });
 
         chatHandler(socket)
         notificationHandler(socket)
