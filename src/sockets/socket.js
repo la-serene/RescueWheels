b095856a753fa8aa38a9c3d883244e35e2b9f4d0
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-25 14:44:34 +0700

    real-time handler prototype for chat and notification

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,8 +1,12 @@
-
+import chatHandler from "./chat.socket.js";
+import notificationHandler from "./notification.socket.js";
 
 export default function initializeSocket(io) {
     io.on('connection', (socket) => {
         console.log(`A user with id ${socket.id} connected`);
+
+        chatHandler(socket)
+        notificationHandler(socket)
 
         socket.on('disconnect', () => {
             console.log('user disconnected');
