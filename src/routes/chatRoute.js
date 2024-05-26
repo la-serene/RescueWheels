4f6d728fa87ea6416926c3da590e1f2b72ec6bb5
Nguyen Cao Ky <caoky2003xx@gmail.com>
2024-05-27 02:19:24 +0700

    remove unnecessary call

--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -1,9 +1,8 @@
 import express from "express"
 import multer from "multer"
-import {getMessageFromUserIdByQuantity, sendMessage} from "../controllers/chatController.js"
+import { getMessageFromUserIdByQuantity } from "../controllers/chatController.js"
 
 const router = express.Router()
 router.get("/getMessageFromUserIdByQuantity/:fromUserId/:quantity", getMessageFromUserIdByQuantity)
-router.post("/", sendMessage)
 
 export default router
