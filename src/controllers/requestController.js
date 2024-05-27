bc5dbfc0edb23ab4c0836b4cc402f13c799d07cd
tranphuong1205 <phuongxxx971022@gmail.com>
2024-05-27 15:30:07 +0700

    add get request by id method

--- src/controllers/requestController.js
+++ src/controllers/requestController.js
@@ -1,35 +1,52 @@
-import Request from "../models/Request.js"
+import Request from "../models/Request.js";
 
 export const createRequest = async (req, res) => {
-    // At a time, user can broadcast only one request.
-    const userId = req.params.userId
-    const {latitude, longitude} = req.body.location
+  // At a time, user can broadcast only one request.
+  const userId = req.params.userId;
+  const { latitude, longitude } = req.body.location;
 
-    try {
-        await new Request({
-            userId: userId,
-            location: {
-                latitude: latitude,
-                longitude: longitude
-            }
-        }).save()
-
-        res.status(200).json({
-            message: "Request created"
-        })
-    } catch (e) {
-        console.log(e.message)
-        res.status(500).json({
-            message: "Failed to create request"
-        })
-    }
-}
-
-export const deleteRequest = async (req, res) => {
-    const requestId = req.params.requestId
-    await Request.findByIdAndDelete(requestId).exec()
+  try {
+    await new Request({
+      userId: userId,
+      location: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    }).save();
 
     res.status(200).json({
-        message: "Request deleted"
-    })
-}
+      message: "Request created",
+    });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({
+      message: "Failed to create request",
+    });
+  }
+};
+
+export const deleteRequest = async (req, res) => {
+  const requestId = req.params.requestId;
+  await Request.findByIdAndDelete(requestId).exec();
+
+  res.status(200).json({
+    message: "Request deleted",
+  });
+};
+export const getRequestById = async (req, res) => {
+  const requestId = req.params.requestId;
+  try {
+    const request = Request.findById(requestId).exec();
+    res.status(200).json({
+      request: request,
+      message: "request retrieved.",
+    });
+  } catch {
+    (e) => {
+      console.log(e.message);
+      res.status(500).json({
+        message: "Request not found.",
+      });
+    };
+  }
+};
